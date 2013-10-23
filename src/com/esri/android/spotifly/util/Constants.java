a3a5ed6f79b8b79a2db9577a79fc83f714061578
Court Fowler <courtf@gmail.com>
2013-10-23 10:41:09 -0700

    constants

---
+++ src/com/esri/android/spotifly/util/Constants.java
@@ -0,0 +1,6 @@
+package com.esri.android.spotifly.util;
+
+public final class Constants {
+    public static final String FLIGHT_STATS_APP_ID = "52f9d24d";
+    public static final String FLIGHT_STATS_APP_KEY = "26f05dd250d70da50a0742a3f3f86cd1";
+}
